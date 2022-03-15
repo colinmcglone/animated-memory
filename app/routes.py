60451ef0bda7330ebf62250e2a223fa070204a40
Colin McGlone <me@colinmcglone.ca>
2022-03-15 11:34:13 -0400

    added git pull hook

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,13 @@
+import git
+
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
 	return "Hello :)"
+
+@app.route('/git/pull')
+def git_pull():
+	repo = git.Repo('~/tracker.colinmcglone.ca')
+	repo.remotes.origin.pull()
